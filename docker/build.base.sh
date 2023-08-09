cefe9d7e6844194b58527ebcf838b4371a53b4b2
Kevin Williams <kevywilly-oribot@gmail.com>
2023-08-09 03:26:01 -0400

    latest

---
+++ docker/build.base.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+echo $1 | grep -E -q '^[0-9.]+$' || die "Numeric argument required, $1 provided"
+
+BASE_TAG="kevywilly/orin-ros2-humble-nav"
+TAG="$BASE_TAG:$1"
+LATEST="$BASE_TAG:latest"
+
+echo $TAG
+echo $LATEST
+
+sudo docker build --network=host -t $TAG -f Dockerfile.base .
+sudo docker tag $TAG $LATEST
