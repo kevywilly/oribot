fb89a73649e6f71368dac93023af3cc1ad818702
Kevin Williams <kevywilly-oribot@gmail.com>
2023-08-09 01:09:46 -0400

    latest

--- src/oribot/oribot/driver_node.py
+++ src/oribot/oribot/driver_node.py
@@ -10,14 +10,17 @@ class DriverNode(Node):
         super().__init__("driver_node")
         self.drivetrain = Drivetrain(self.get_logger(), 2, 2)
         self.create_subscription(Twist, "/oribot/cmd_vel", self.handle_cmd_vel, 10)
-
+        #self.twist_timer = self.create_timer(0.1, self.twist_callback)
+        
     def log(self, txt: str):
         self.get_logger().info(txt)
 
     def handle_cmd_vel(self, msg: Twist):
-        
         self.drivetrain.drive_twist(msg)
-        #self.drivetrain.drive(msg.linear.x, msg.angular.z)
+
+    def twist_callback(self):
+        pass
+            
 
     def shutdown(self):
         self.drivetrain.stop()
